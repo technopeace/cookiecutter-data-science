b361d82db1bf341b915c745cfdd4fcfeadc1c076
technopeace <52013213+technopeace@users.noreply.github.com>
2025-01-08 22:29:02 +0300

    Update post_gen_project.py
    bug fix: string to json

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -93,6 +93,10 @@ for generated_path in Path("{{ cookiecutter.module_name }}").iterdir():
 create_notebook_var = "{{ cookiecutter.create_notebook }}"
 notebook_name = "{{ cookiecutter.notebook_name }}"
 use_yaml_parameters = "{{ cookiecutter.use_yaml_parameters }}"
+try:
+    use_yaml_parameters = json.loads(use_yaml_parameters)
+except json.JSONDecodeError as e:
+    print(f"Error decoding JSON string: {e}")
 file_types_var = "{{ cookiecutter.file_types }}"
 if list(use_yaml_parameters.keys())[0] == 'Yes':
     yaml_path = use_yaml_parameters['Yes']['yaml_path']
