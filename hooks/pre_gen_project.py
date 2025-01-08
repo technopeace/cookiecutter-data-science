abf60ffbc215946bfc91599cf249fec6e93b2e47
technopeace <52013213+technopeace@users.noreply.github.com>
2025-01-08 21:21:07 +0300

    Update pre_gen_project.py
    reverting

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,20 +1,4 @@
 # Functions here run before the project is generated.
-import sys
-
-def main():
-    # Kullanıcının `use_yaml_parameters` seçimini al
-    use_yaml_parameters = "{{ cookiecutter.use_yaml_parameters }}"
-    
-    # Eğer 'No' seçilmişse, kullanıcıya YAML yolu sormayı atla
-    if use_yaml_parameters.lower() != "Yes":
-        sys.stdout.write("Skipping YAML path as 'use_yaml_parameters' is 'No'.\n")
-        # Burada, `yaml_path` boş bir değerle geçersiz kılınabilir.
-        cookiecutter_context = {
-            "yaml_path": ""
-        }
-
-if __name__ == "__main__":
-    main()
 
 # For the use of these hooks, see
 # See https://cookiecutter.readthedocs.io/en/1.7.2/advanced/hooks.html
